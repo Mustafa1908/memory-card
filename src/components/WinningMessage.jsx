d1b6f21be6357f2ec394576c34ded73dfca629da
Mustafa Özkara <mustafaozkara@outlook.fr>
2024-05-27 18:41:00 +0200

    Add class to span and remove last paragraph

--- src/components/WinningMessage.jsx
+++ src/components/WinningMessage.jsx
@@ -9,11 +9,10 @@ export default function WinningMessage() {
           src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNG5sMTd2YXhpdHY1Nm53YjB5eDI0bWN0ZDJoaWdrM25sN2Q5ZG9paCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9pxrEmGb1n1JklkSnG/giphy.gif"
           alt=""
         />
-        <span>You win congratulations!</span>
+        <span className="winningText">You win congratulations!</span>
         <button className="closeWin" autoFocus>
-          Retry
+          Retry again
         </button>
-        <p>This modal dialog has a groovy backdrop!</p>
       </dialog>
     </>
   );
