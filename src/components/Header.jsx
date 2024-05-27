51e1ac157b8980dc128913fc39531c556deaa78e
Mustafa Özkara <mustafaozkara@outlook.fr>
2024-05-27 18:39:36 +0200

    Add boolean state varaible, playMusic function and button container for audio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
+import Test from "./ChooseDifficulty";
 import "../styles/Header.css";
 
 export default function Header() {
+  const [music, setMusic] = useState(false);
+
+  const playMusic = () => {
+    let pokemonMusic = document.querySelector(".pokemonMusic");
+
+    if (music === true) {
+      pokemonMusic.pause();
+      setMusic(false);
+    } else {
+      pokemonMusic.play();
+      setMusic(true);
+    }
+  };
+
   return (
     <>
       <header className="header">
@@ -11,10 +27,19 @@ export default function Header() {
             className="pokeBallPicture"
           />
           <h1 className="headerOne">PokeCard</h1>
+          <button
+            className="material-symbols-outlined musicButton"
+            onClick={playMusic}
+          >
+            music_note{" "}
+            <audio autoPlay loop className="pokemonMusic">
+              <source src="../audio/pokemon-music.mp4" type="audio/mpeg" />
+            </audio>
+          </button>
         </div>
         <div className="headerTextContainer">
-          <span className="headerText">Score: 0</span>
-          <span className="headerText">Best Score: 13</span>
+          <span className="currentScore">Score: 0</span>
+          <span className="bestScore">Best Score: 0</span>
         </div>
       </header>
     </>
