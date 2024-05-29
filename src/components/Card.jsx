0e0ba33e894a221252dcb4e91e78e00a081a42dc
Mustafa Özkara <mustafaozkara@outlook.fr>
2024-05-29 15:49:15 +0200

    Add alt attribute to img element

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,11 @@ import "../styles/Card.css";
 export default function Card({ pokemonName, pokemonImage }) {
   return (
     <div className="cardContainer">
-      <img className="pokemonPicture" src={pokemonImage} />
+      <img
+        className="pokemonPicture"
+        src={pokemonImage}
+        alt="pokemon picture"
+      />
       <span className="pokemonName">{pokemonName}</span>
     </div>
   );
